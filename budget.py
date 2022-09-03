d41081bd519d69c6c810ad2ec8acb4b5428c5334
youssef ben rejeb <youssef.benrejeb@gmail.com>
2022-09-04 00:12:39 +0100

    Les tests passe si on les executent un par un, mais si on les execute ensemble on aura une erreur ?

--- budget.py
+++ budget.py
@@ -12,7 +12,7 @@ def create_spend_chart(categories):
     pass
 
 
-if __name__ == "__main__":
-    scc = Category()
-    scc.deposit("amount_example", "description_example")
-    print(scc.deposit("amount_example", "description_example"))
+# if __name__ == "__main__":
+#     scc = Category()
+#     scc.deposit("amount_example", "description_example")
+#     print(scc.deposit("amount_example", "description_example"))
--- test_module.py
+++ test_module.py
@@ -9,20 +9,23 @@ def input_value():
     return value
 
 
+def test_two_deposit(input_value):
+    l = [{"amount": input_value[0], "description": input_value[1]},
+         {"amount": input_value[2], "description": input_value[3]}]
+    sc2 = Category()
+
+    sc2.deposit(input_value[0], input_value[1])
+
+    assert sc2.deposit(input_value[2], input_value[3]) == l
+
+
 # @pytest.mark.skip
 def test_one_deposit(input_value):
+
     sc = Category()
+    l=[{'amount': input_value[0], 'description': input_value[1]}]
 
-    assert sc.deposit(input_value[0], input_value[1]) == [{'amount': input_value[0], 'description': input_value[1]}]
-
-
-def test_two_deposit(input_value):
-    l = []
-    sc = Category()
-    l.append(sc.deposit(input_value[0], input_value[1]))
-    l.append(sc.deposit(input_value[2], input_value[3]))
-
-    assert l == [{'amount': input_value[0], 'description': input_value[1]}, {'amount': input_value[2], 'description': input_value[3]}]
+    assert sc.deposit(input_value[0], input_value[1]) == l
 
 # if __name__ == "__main__":
 #     test__deposit(input_value)
